b003589188d897bab45b3beeb185e9892738f852
dkolomiichenko <dkolomiichenko@DKOLOMIICHENKO9>
2019-06-25 16:13:13 +0300

    Added Domain project

---
+++ Ordering/Ordering.Domain/AggregatesModel/BuyerAggregate/CardType.cs
@@ -0,0 +1,20 @@
+﻿using Ordering.Domain.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordering.Domain.AggregatesModel.BuyerAggregate
+{
+    public class CardType
+         : Enumeration
+    {
+        public static CardType Amex = new CardType(1, "Amex");
+        public static CardType Visa = new CardType(2, "Visa");
+        public static CardType MasterCard = new CardType(3, "MasterCard");
+
+        public CardType(int id, string name)
+            : base(id, name)
+        {
+        }
+    }
+}
---
+++ Ordering/Ordering.Domain/SeedWork/IAggregateRoot.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordering.Domain.SeedWork
+{
+    public interface IAggregateRoot
+    {
+    }
+}
